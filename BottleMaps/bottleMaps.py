6fbd765bd52a24778b0cd6baa44250f3f978dcdb
LeonardPannetier <167757836+LeonardPannetier@users.noreply.github.com>
2024-06-15 09:30:59 +0200

    l

--- BottleMaps/bottleMaps.py
+++ BottleMaps/bottleMaps.py
@@ -1,7 +1,13 @@
+import sys
+import time
+import numpy
+from PySide6 import QtCore, QtGui
+import PySide6 as Qwt
+
 
 
 class BottleMaps:
     
 
-    def save_map():
-        pass
+    def save_map(self):
+        self.label.setPixmap(QtGui.QPixmap(_fromUtf8("Images/10.jpg")))
